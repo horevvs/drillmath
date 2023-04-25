9aff12bd8a2fea45a4f341b7b8566ca77604f956
Vladimir Horev <VShorev@yandex.ru>
2023-04-25 18:30:59 +0300

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,31 @@ import { NavLink } from 'react-router-dom';
 function Home() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container px-lg-5">
           <a class="navbar-brand" href="#!">Инженерные расчеты</a>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item m-1"> <NavLink to={'/Blog'}> Глушение </NavLink> </li>
-              <li class="nav-item m-1"> <NavLink to={'/Chats'}> Цементаж </NavLink> </li>
-              <li class="nav-item m-2"> <NavLink to={'/Profiles'}> Буровые растворы </NavLink> </li>
-              <li class="nav-item m-1"> <NavLink to={'/Metric'}> Перевод величин </NavLink> </li>
+              <li class="nav-item m-1 "> <NavLink to={'/Blog'}  
+              style={({ isActive }) => ({color: isActive ? '#ffff' : '#ffff', 
+              textDecoration: isActive ? 'none' : 'none'})}> Глушение </NavLink> </li>
+             
+              <li class="nav-item m-1"> <NavLink to={'/Chats'} 
+              style={({ isActive }) => ({color: isActive ? '#ffff' : '#ffff', textDecoration: isActive ? 'none' : 'none'})}> Цементаж </NavLink> </li>
+            
+              <li class="nav-item m-1"> <NavLink to={'/Profiles'}
+               style={({ isActive }) => ({color: isActive ? '#ffff' : '#ffff', textDecoration: isActive ? 'none' : 'none'})}> Буровые растворы </NavLink> </li>
+             
+             
+              <li class="nav-item m-1"> <NavLink to={'/Metric'}  
+               style={({ isActive }) => ({color: isActive ? '#ffff' : '#ffff', textDecoration: isActive ? 'none' : 'none'})}
+              > Перевод величин </NavLink> </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <div>3</div>
+      
     </>
   )
 }
--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -8,11 +8,28 @@ function Profiles() {
         <div class="container px-lg-5">
           <a class="navbar-brand" href="#!">Инженерные расчеты</a>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
-              <li class="nav-item m-1"> <NavLink to={'/Blog'}> Глушение </NavLink> </li>
-              <li class="nav-item m-1"> <NavLink to={'/Chats'}> Цементаж </NavLink> </li>
-              <li class="nav-item m-2"> <NavLink to={'/Profiles'}> Буровые растворы </NavLink> </li>
-              <li class="nav-item m-1"> <NavLink to={'/Metric'}> Перевод величин </NavLink> </li>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item m-1 "> <NavLink to={'/Blog'}  
+              style={({ isActive }) => 
+              ({color: isActive ? '#ffff' : '#ffff',
+                textDecoration: isActive ? 'none' : 'none'
+              })}> 
+              
+              Глушение </NavLink> </li>
+             
+              <li class="nav-item m-1"> <NavLink to={'/Chats'} 
+              style={({ isActive }) => ({color: isActive ? '#ffff' : '#ffff',
+              textDecoration: isActive ? 'none' : 'none'})}> Цементаж </NavLink> </li>
+            
+              <li class="nav-item m-1"> <NavLink to={'/Profiles'}
+               style={({ isActive }) => ({color: isActive ? '#ffff' : '#ffff',
+               textDecoration: isActive ? 'none' : 'none'})}> Буровые растворы </NavLink> </li>
+             
+             
+              <li class="nav-item m-1"> <NavLink to={'/Metric'}  
+               style={({ isActive }) => ({color: isActive ? '#ffff' : '#ffff',
+               textDecoration: isActive ? 'none' : 'none'})}
+              > Перевод величин </NavLink> </li>
             </ul>
           </div>
         </div>
