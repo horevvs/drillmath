9e42314d647b4f7662708ccd5dd9c8b3317312a0
Vladimir Horev <VShorev@yandex.ru>
2023-04-29 17:18:15 +0300

    d

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -5,17 +5,14 @@ import './App2.css'
 
 function Profiles() {
 
-
-
-
-   const divStyle = {
-   height: '100vh'
+  const divStyle = {
+    height: '100vh'
   };
 
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container px-lg-5">
+        <div class="container ">
           <a class="navbar-brand" href="#!">Инженерные расчеты</a>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -54,11 +51,23 @@ function Profiles() {
 
 
 
-      <div style={divStyle} class="text-bg-light mx-5 bg-opacity-25 ">
-        445
-   
+      <div style={divStyle} class="bg-info mx-5 bg-opacity-25 ">
+        <p class="mx-5 py-3 fs-5"> 1. Расчет  гидростатического давления</p>
 
-
+        <form lass="">
+          <div class="form-group mx-5 pt-2 w-25">
+            <label for="formGroupExampleInput">Плотность промывочной жидкости (г/см<sup>3</sup>) </label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Введите значение" />
+          </div>
+          <div class="form-group mx-5  pt-2 w-25">
+            <label for="formGroupExampleInput2 pt-5" >Глубина (м) </label>
+            <input type="text" class="form-control " id="formGroupExampleInput2" placeholder="Введите значение" />
+          </div>
+          <div class="form-group mx-5 pt-2  w-25">
+            <label for="formGroupExampleInput2">Глубина (м) </label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Введите значение" />
+          </div>
+        </form>
 
 
 
