7207f4b4544624372c6e8386b045acb18a9b2835
Vladimir horev <Vshorev@yandex.ru>
2023-05-02 00:51:03 +0700

    добавил обработчик

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './App2.css'
-import Popup from 'reactjs-popup';
-
+import Button from 'react-bootstrap/Button';
 
 
 
@@ -11,73 +10,109 @@ import Popup from 'reactjs-popup';
 
 function Profiles() {
 
-  const divStyle = {
-    height: '100vh'
-  };
-
-  return (
-    <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container ">
-          <a class="navbar-brand" href="#!">Инженерные расчеты</a>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item m-1 "> <NavLink to={'/Blog'}
-                style={({ isActive }) =>
-                ({
-                  color: isActive ? '#ffff' : '#ffff',
-                  textDecoration: isActive ? 'none' : 'none'
-                })}>
-
-                Глушение </NavLink> </li>
-
-              <li class="nav-item m-1"> <NavLink to={'/Chats'}
-                style={({ isActive }) => ({
-                  color: isActive ? '#ffff' : '#ffff',
-                  textDecoration: isActive ? 'none' : 'none'
-                })}> Цементаж </NavLink> </li>
-
-              <li class="nav-item m-1"> <NavLink to={'/Profiles'}
-                style={({ isActive }) => ({
-                  color: isActive ? '#ffff' : '#ffff',
-                  textDecoration: isActive ? 'none' : 'none'
-                })}> Буровые растворы </NavLink> </li>
+  
+  const [inputs, setInputs] = useState([])
+  const [inputs2, setInputs2] = useState([])
 
 
-              <li class="nav-item m-1"> <NavLink to={'/Metric'}
-                style={({ isActive }) => ({
-                  color: isActive ? '#ffff' : '#ffff',
-                  textDecoration: isActive ? 'none' : 'none'
-                })}
-              > Перевод величин </NavLink> </li>
-            </ul>
-          </div>
+  const getValues = () => {
+    alert(inputs)
+  }
+
+  const getValues2 = () => {
+    alert(inputs2)
+  }
+
+
+
+
+
+
+const divStyle = {
+  height: '100vh'
+};
+
+return (
+  <>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container ">
+        <a class="navbar-brand" href="#!">Инженерные расчеты</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item m-1 "> <NavLink to={'/Blog'}
+              style={({ isActive }) =>
+              ({
+                color: isActive ? '#ffff' : '#ffff',
+                textDecoration: isActive ? 'none' : 'none'
+              })}>
+
+              Глушение </NavLink> </li>
+
+            <li class="nav-item m-1"> <NavLink to={'/Chats'}
+              style={({ isActive }) => ({
+                color: isActive ? '#ffff' : '#ffff',
+                textDecoration: isActive ? 'none' : 'none'
+              })}> Цементаж </NavLink> </li>
+
+            <li class="nav-item m-1"> <NavLink to={'/Profiles'}
+              style={({ isActive }) => ({
+                color: isActive ? '#ffff' : '#ffff',
+                textDecoration: isActive ? 'none' : 'none'
+              })}> Буровые растворы </NavLink> </li>
+
+
+            <li class="nav-item m-1"> <NavLink to={'/Metric'}
+              style={({ isActive }) => ({
+                color: isActive ? '#ffff' : '#ffff',
+                textDecoration: isActive ? 'none' : 'none'
+              })}
+            > Перевод величин </NavLink> </li>
+          </ul>
         </div>
-      </nav>
-
-
-
-      <div style={divStyle} class="bg-info mx-5 bg-opacity-25 ">
-        <p class="mx-5 py-3 fs-5"> 1. Расчет  гидростатического давления</p>
-
-        <form lass="">
-          <div class="form-group mx-5 pt-2 w-25">
-            <label for="formGroupExampleInput">Плотность промывочной жидкости (г/см<sup>3</sup>) </label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Введите значение" />
-          </div>
-          <div class="form-group mx-5  pt-2 w-25">
-            <label for="formGroupExampleInput2 pt-5" >Глубина (м) </label>
-            <input type="text" class="form-control " id="formGroupExampleInput2" placeholder="Введите значение" />
-          </div>
-          <div class="form-group mx-5 pt-2  w-25">
-            <label for="formGroupExampleInput2">Глубина (м) </label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Введите значение" />
-          </div>
-        </form>
-
       </div>
-    </>
-  )
+    </nav>
+
+
+
+    <div style={divStyle} class="bg-info mx-5 bg-opacity-25 ">
+      <p class="mx-5 py-3 fs-5"> 1. Расчет  гидростатического давления</p>
+
+      <form lass="">
+        <div class="form-group mx-5 pt-2 w-25">
+          <label for="formGroupExampleInput">Плотность промывочной жидкости (г/см<sup>3</sup>) </label>
+          <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Введите значение" value={inputs} onChange={(e) => setInputs(e.target.value)} />
+        </div>
+        <div class="form-group mx-5  pt-2 w-25">
+          <label for="formGroupExampleInput2 pt-5" >Глубина (м) </label>
+          <input type="text" class="form-control " id="formGroupExampleInput2" placeholder="Введите значение"  value={inputs2} onChange={(e) => setInputs2(e.target.value)}/>
+        </div>
+        <div class="form-group mx-5 pt-2  w-25">
+          <label for="formGroupExampleInput2">Глубина (м) </label>
+          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Введите значение" />
+        </div>
+
+
+        <div class='mx-5 py-4'>
+          <Button onClick={() => getValues()} variant="primary" size="sm" active>
+            Выполнить расчет
+          </Button>
+
+          <Button onClick={() => getValues2()} variant="primary" size="sm" active>
+            Выполнить расчет
+          </Button>
+
+
+
+        </div>
+
+      </form>
+
+
+
+
+    </div>
+  </>
+)
 }
 
 export default Profiles;
